873fed8c5923da77be75ad1bac28bababc10a4a8
CristianPalta1 <critian.palta.c@uniautonoma.edu.co>
2021-11-13 22:27:47 -0500

    Commit Carousel

---
+++ src/components/carousel/Carousel.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+//Styles
+import "./styles/styles.scss"
+
+export default function Carousel() {
+  return (
+    <div className="slider">
+      <div className = "text">
+          <div className="content"><p>Contenido</p></div>
+      </div>
+      <div className = "change-img">
+          puntos
+      </div>
+    </div>
+  );
+}
--- src/views/home/Home.js
+++ src/views/home/Home.js
@@ -1,146 +1,151 @@
 import React from "react";
 
-import ImgagePrueba from "../../assets/images/thumbnails/deadpool.png"
+import ImgagePrueba from "../../assets/images/thumbnails/deadpool.png";
 
+//Components
+import Carousel from "../../components/carousel/Carousel";
 
 import "./styles/styles.scss";
 
 function Home() {
   return (
-    <section class="post-list">
-      <a href="" class="post">
-        <figure class="post-image">
-          <img src={ImgagePrueba} />
-        </figure>
-        <span class="post-overlay">
-          <p>
-            <span class="post-likes">150</span>
-            <span class="post-comments">10</span>
-          </p>
-        </span>
-      </a>
-      <a href="" class="post">
-        <figure class="post-image">
-        <img src={ImgagePrueba} />
-        </figure>
-        <span class="post-overlay">
-          <p>
-            <span class="post-likes">150</span>
-            <span class="post-comments">10</span>
-          </p>
-        </span>
-      </a>
-      <a href="" class="post">
-        <figure class="post-image">
-          <img src={ImgagePrueba} />
-        </figure>
-        <span class="post-overlay">
-          <p>
-            <span class="post-likes">150</span>
-            <span class="post-comments">10</span>
-          </p>
-        </span>
-      </a>
-      <a href="" class="post">
-        <figure class="post-image">
-          <img src={ImgagePrueba} />
-        </figure>
-        <span class="post-overlay">
-          <p>
-            <span class="post-likes">150</span>
-            <span class="post-comments">10</span>
-          </p>
-        </span>
-      </a>
-      <a href="" class="post">
-        <figure class="post-image">
-          <img src={ImgagePrueba} />
-        </figure>
-        <span class="post-overlay">
-          <p>
-            <span class="post-likes">150</span>
-            <span class="post-comments">10</span>
-          </p>
-        </span>
-      </a>
-      <a href="" class="post">
-        <figure class="post-image">
-          <img src={ImgagePrueba} />
-        </figure>
-        <span class="post-overlay">
-          <p>
-            <span class="post-likes">150</span>
-            <span class="post-comments">10</span>
-          </p>
-        </span>
-      </a>
-      <a href="" class="post">
-        <figure class="post-image">
-          <img src={ImgagePrueba} />
-        </figure>
-        <span class="post-overlay">
-          <p>
-            <span class="post-likes">150</span>
-            <span class="post-comments">10</span>
-          </p>
-        </span>
-      </a>
-      <a href="" class="post">
-        <figure class="post-image">
-          <img src={ImgagePrueba} />
-        </figure>
-        <span class="post-overlay">
-          <p>
-            <span class="post-likes">150</span>
-            <span class="post-comments">10</span>
-          </p>
-        </span>
-      </a>
-      <a href="" class="post">
-        <figure class="post-image">
-          <img src={ImgagePrueba} />
-        </figure>
-        <span class="post-overlay">
-          <p>
-            <span class="post-likes">150</span>
-            <span class="post-comments">10</span>
-          </p>
-        </span>
-      </a>
-      <a href="" class="post">
-        <figure class="post-image">
-          <img src={ImgagePrueba} />
-        </figure>
-        <span class="post-overlay">
-          <p>
-            <span class="post-likes">150</span>
-            <span class="post-comments">10</span>
-          </p>
-        </span>
-      </a>
-      <a href="" class="post">
-        <figure class="post-image">
-          <img src={ImgagePrueba} />
-        </figure>
-        <span class="post-overlay">
-          <p>
-            <span class="post-likes">150</span>
-            <span class="post-comments">10</span>
-          </p>
-        </span>
-      </a>
-      <a href="" class="post">
-        <div class="post-image">
-          <img src={ImgagePrueba} />
-        </div>
-        <span class="post-overlay">
-          <p>
-            <span class="post-likes">150</span>
-            <span class="post-comments">10</span>
-          </p>
-        </span>
-      </a>
-    </section>
+    <>
+      <Carousel />
+      <section class="post-list">
+        <a href="" class="post">
+          <figure class="post-image">
+            <img src={ImgagePrueba} />
+          </figure>
+          <span class="post-overlay">
+            <p>
+              <span class="post-likes">150</span>
+              <span class="post-comments">10</span>
+            </p>
+          </span>
+        </a>
+        <a href="" class="post">
+          <figure class="post-image">
+            <img src={ImgagePrueba} />
+          </figure>
+          <span class="post-overlay">
+            <p>
+              <span class="post-likes">150</span>
+              <span class="post-comments">10</span>
+            </p>
+          </span>
+        </a>
+        <a href="" class="post">
+          <figure class="post-image">
+            <img src={ImgagePrueba} />
+          </figure>
+          <span class="post-overlay">
+            <p>
+              <span class="post-likes">150</span>
+              <span class="post-comments">10</span>
+            </p>
+          </span>
+        </a>
+        <a href="" class="post">
+          <figure class="post-image">
+            <img src={ImgagePrueba} />
+          </figure>
+          <span class="post-overlay">
+            <p>
+              <span class="post-likes">150</span>
+              <span class="post-comments">10</span>
+            </p>
+          </span>
+        </a>
+        <a href="" class="post">
+          <figure class="post-image">
+            <img src={ImgagePrueba} />
+          </figure>
+          <span class="post-overlay">
+            <p>
+              <span class="post-likes">150</span>
+              <span class="post-comments">10</span>
+            </p>
+          </span>
+        </a>
+        <a href="" class="post">
+          <figure class="post-image">
+            <img src={ImgagePrueba} />
+          </figure>
+          <span class="post-overlay">
+            <p>
+              <span class="post-likes">150</span>
+              <span class="post-comments">10</span>
+            </p>
+          </span>
+        </a>
+        <a href="" class="post">
+          <figure class="post-image">
+            <img src={ImgagePrueba} />
+          </figure>
+          <span class="post-overlay">
+            <p>
+              <span class="post-likes">150</span>
+              <span class="post-comments">10</span>
+            </p>
+          </span>
+        </a>
+        <a href="" class="post">
+          <figure class="post-image">
+            <img src={ImgagePrueba} />
+          </figure>
+          <span class="post-overlay">
+            <p>
+              <span class="post-likes">150</span>
+              <span class="post-comments">10</span>
+            </p>
+          </span>
+        </a>
+        <a href="" class="post">
+          <figure class="post-image">
+            <img src={ImgagePrueba} />
+          </figure>
+          <span class="post-overlay">
+            <p>
+              <span class="post-likes">150</span>
+              <span class="post-comments">10</span>
+            </p>
+          </span>
+        </a>
+        <a href="" class="post">
+          <figure class="post-image">
+            <img src={ImgagePrueba} />
+          </figure>
+          <span class="post-overlay">
+            <p>
+              <span class="post-likes">150</span>
+              <span class="post-comments">10</span>
+            </p>
+          </span>
+        </a>
+        <a href="" class="post">
+          <figure class="post-image">
+            <img src={ImgagePrueba} />
+          </figure>
+          <span class="post-overlay">
+            <p>
+              <span class="post-likes">150</span>
+              <span class="post-comments">10</span>
+            </p>
+          </span>
+        </a>
+        <a href="" class="post">
+          <div class="post-image">
+            <img src={ImgagePrueba} />
+          </div>
+          <span class="post-overlay">
+            <p>
+              <span class="post-likes">150</span>
+              <span class="post-comments">10</span>
+            </p>
+          </span>
+        </a>
+      </section>
+    </>
   );
 }
 
