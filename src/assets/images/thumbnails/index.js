e4e24ec83648f0562917dba6b2c33e68df30e1d3
Sebastian Pea <juansebastianp21@gmail.com>
2021-11-13 21:18:42 -0500

    category bar list initial display

---
+++ src/assets/images/thumbnails/index.js
@@ -0,0 +1,19 @@
+import Bohemian from "./bohemian-rhapsody.jpg";
+import Avengers from "./avengers-infinity-war.png";
+import Deadpool from "./deadpool.png";
+import FirstMan from "./first-man.jpg";
+import HanSolo from "./han-solo.jpg";
+import Incredibles from "./incredibles.jpg";
+import Jurassic from "./jurassic-world.jpg";
+import Venom from "./venom.jpg";
+
+export {
+  Bohemian,
+  Avengers,
+  Deadpool,
+  FirstMan,
+  HanSolo,
+  Incredibles,
+  Jurassic,
+  Venom,
+};
